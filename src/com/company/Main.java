ff4f567da5d18777c6c720b4322c0d20db2fe034
dan525555 <77828669+dan525555@users.noreply.github.com>
2023-03-02 15:38:58 +0100

    Zrobienie zadania 1

---
+++ src/com/company/Main.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("nazwa grupy:dieArbeitslosen\nTeamLeader: dan525555\nrola: tester");
+    }
+}
